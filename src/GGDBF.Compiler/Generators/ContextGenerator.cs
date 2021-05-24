14e830c9f95ac24bc6dff06924453d9a7009c5c8
HelloKitty <andrew.blakely@ymail.com>
2021-05-24 12:05:01 -0500

    Added ContextGenerator and Analyzer nugets

---
+++ src/GGDBF.Compiler/Generators/ContextGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace GGDBF
+{
+	[Generator]
+	public class ContextGenerator : ISourceGenerator
+	{
+		public void Initialize(GeneratorInitializationContext context)
+		{
+
+		}
+
+		public void Execute(GeneratorExecutionContext context)
+		{
+
+		}
+	}
+}
