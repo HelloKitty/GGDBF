39889edd80736404c3662959d90d567c212e0253
HelloKitty <andrew.blakely@ymail.com>
2021-06-05 15:45:45 -0500

    Added ISourceEmitter

---
+++ src/GGDBF.Compiler/Emitter/ISourceEmitter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF.Compiler
+{
+	public interface ISourceEmitter
+	{
+		string Emit();
+
+		void Emit(StringBuilder builder);
+	}
+}
