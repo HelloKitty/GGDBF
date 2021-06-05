1d6d5c8ce8dc0239e8efa271d8c2df061c099c14
HelloKitty <andrew.blakely@ymail.com>
2021-06-05 16:08:46 -0500

    Added missing Test proj

---
+++ tests/GGDBF.Compiler.ManualTest/TestContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using TestNamespace;
+
+namespace GGDBF
+{
+	[RequiredDataModel(typeof(string))]
+	[RequiredDataModel(typeof(TestModelType))]
+	public partial class TestContext
+	{
+
+	}
+}
+
+namespace TestNamespace
+{
+	public class TestModelType
+	{
+
+	}
+}
