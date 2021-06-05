0cc78446c1bee4e9a2fdd9ef381d204f5f9111c3
HelloKitty <andrew.blakely@ymail.com>
2021-06-05 16:08:38 -0500

    Added demo Generator

--- src/GGDBF.Compiler/Generators/ContextGenerator.cs
+++ src/GGDBF.Compiler/Generators/ContextGenerator.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
+using GGDBF.Compiler;
+using Glader.Essentials;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 
@@ -16,7 +20,67 @@ namespace GGDBF
 
 		public void Execute(GeneratorExecutionContext context)
 		{
+			try
+			{
+				ExecuteGenerator(context);
+			}
+			catch(System.Reflection.ReflectionTypeLoadException e)
+			{
+				context.AddSource("Error.txt", $"{e}\n\nLoader: {e.LoaderExceptions.Select(ex => ex.ToString()).Aggregate((s1, s2) => $"{s1}\n{s2}")}");
+				throw;
+			}
+			catch(Exception e)
+			{
+				context.AddSource("Error.txt", e.ToString());
 
+				try
+				{
+					context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("FCC001", "Compiler Failure", $"Error: {e.GetType().Name}. Failed: {e.Message} Stack: {{0}}", "Error", DiagnosticSeverity.Error, true), Location.None, BuildStackTrace(e)));
+				}
+				finally
+				{
+					throw e;
+				}
+			}
+		}
+
+		private static void ExecuteGenerator(GeneratorExecutionContext context)
+		{
+			INamedTypeSymbol[] symbols = context
+				.GetAllTypes()
+				.ToArray();
+
+			//This finds all the context symbols trying to be generated
+			var dataContextSymbols = symbols
+				.Where(s => s.HasAttributeExact<RequiredDataModelAttribute>())
+				.ToArray();
+
+			foreach (var contextSymbol in dataContextSymbols)
+			{
+				StringBuilder builder = new StringBuilder();
+				UsingsEmitter usingsEmitter = new();
+
+				foreach (var type in contextSymbol
+					.GetAttributesExact<RequiredDataModelAttribute>()
+					.Select(a => (INamedTypeSymbol)a.ConstructorArguments.First().Value))
+				{
+					if (type.ContainingNamespace != null)
+						usingsEmitter.AddNamespace(type.ContainingNamespace.FullNamespaceString());
+				}
+
+				usingsEmitter.Emit(builder);
+				context.AddSource(contextSymbol.Name, builder.ToString());
+			}
+		}
+
+		private static string BuildStackTrace(Exception e)
+		{
+			return e.StackTrace
+				.Replace('{', ' ')
+				.Replace('}', ' ')
+				.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+				.Skip(5)
+				.Aggregate((s1, s2) => $"{s1} {s2}");
 		}
 	}
 }
