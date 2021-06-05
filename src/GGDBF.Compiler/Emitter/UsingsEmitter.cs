55b7940a80189ba1b9a0f98b92067998f85059a7
HelloKitty <andrew.blakely@ymail.com>
2021-06-05 15:45:53 -0500

    Created UsingsEmitter

---
+++ src/GGDBF.Compiler/Emitter/UsingsEmitter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF.Compiler
+{
+	public sealed class UsingsEmitter : ISourceEmitter
+	{
+		private HashSet<string> RequestedNamespaces { get; } = new();
+
+		private const string USING_DIRECTIVE_FORMAT_TEXT = "using {0};";
+
+		public string Emit()
+		{
+			var builder = new StringBuilder();
+			Emit(builder);
+			return builder.ToString();
+		}
+
+		public void Emit(StringBuilder builder)
+		{
+			if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+			foreach (var entry in RequestedNamespaces)
+			{
+				builder.AppendFormat(USING_DIRECTIVE_FORMAT_TEXT, entry);
+				builder.Append("\n");
+			}
+		}
+
+		/// <summary>
+		/// Adds the requested namespace to the using emitter.
+		/// </summary>
+		/// <param name="namespace">The namespace to add.</param>
+		public void AddNamespace(string @namespace)
+		{
+			if (string.IsNullOrWhiteSpace(@namespace)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(@namespace));
+			RequestedNamespaces.Add(@namespace);
+		}
+	}
+}
