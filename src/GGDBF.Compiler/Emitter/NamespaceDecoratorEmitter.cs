3b74f151bd7dd842647cd30fa60c7580e9db376d
HelloKitty <andrew.blakely@ymail.com>
2021-06-07 18:00:01 -0500

    Added NamespaceDecoratorEmitter

---
+++ src/GGDBF.Compiler/Emitter/NamespaceDecoratorEmitter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF.Compiler
+{
+	public sealed class NamespaceDecoratorEmitter : ISourceEmitter
+	{
+		private ISourceEmitter Emitter { get; }
+
+		private string NamespaceName { get; }
+
+		public NamespaceDecoratorEmitter(ISourceEmitter emitter, string namespaceName)
+		{
+			Emitter = emitter ?? throw new ArgumentNullException(nameof(emitter));
+			NamespaceName = namespaceName ?? throw new ArgumentNullException(nameof(namespaceName));
+		}
+
+		public void Emit(StringBuilder builder)
+		{
+			builder.Append($"namespace {NamespaceName}\n{{");
+			Emitter.Emit(builder);
+			builder.Append($"\n}}");
+		}
+	}
+}
