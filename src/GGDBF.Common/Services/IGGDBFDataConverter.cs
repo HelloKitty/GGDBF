ef60333358ccffb90b0a9914b4caf3cd53ac011d
HelloKitty <andrew.blakely@ymail.com>
2021-06-08 02:48:09 -0500

    Added IGGDBFDataConverter

---
+++ src/GGDBF.Common/Services/IGGDBFDataConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF
+{
+	/// <summary>
+	/// GGDBF's conversion service.
+	/// Converts a table Type into a serializable <see cref="GGDBFTable{TPrimaryKeyType,TModelType}"/>.
+	/// </summary>
+	public interface IGGDBFDataConverter
+	{
+		/// <summary>
+		/// Converts the internal data source's representation of the table to <see cref="GGDBFTable{TPrimaryKeyType,TModelType}"/>.
+		/// </summary>
+		/// <typeparam name="TPrimaryKeyType">The key type of the table.</typeparam>
+		/// <typeparam name="TModelType">The model type of the table.</typeparam>
+		/// <param name="keyResolutionFunction">Optional key resolution function that can retrieve the key from the model.</param>
+		/// <returns>A serializable <see cref="GGDBFTable{TPrimaryKeyType,TModelType}"/> representation.</returns>
+		GGDBFTable<TPrimaryKeyType, TModelType> Convert<TPrimaryKeyType, TModelType>(Func<TModelType, TPrimaryKeyType> keyResolutionFunction = null);
+	}
+}
