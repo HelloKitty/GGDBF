3535341884cc6464a14ec45ddf9aaab415d1509c
HelloKitty <andrew.blakely@ymail.com>
2021-06-08 02:19:28 -0500

    Remove field support for DatabaseKeyHintAttribute

--- src/GGDBF.Common/Attributes/DatabaseKeyHintAttribute.cs
+++ src/GGDBF.Common/Attributes/DatabaseKeyHintAttribute.cs
@@ -8,7 +8,7 @@ namespace GGDBF
 	/// Attribute that can be applied to properties to indicate (or hint) that a property is the primary key for the data model.
 	/// In cases where typical annotations are not or cannot be used.
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 	public sealed class DatabaseKeyHintAttribute : Attribute
 	{
 		/// <summary>
