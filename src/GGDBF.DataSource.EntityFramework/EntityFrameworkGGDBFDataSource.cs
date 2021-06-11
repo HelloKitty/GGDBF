c3a7ad481be491835e5dd7fa4e3e9cc270f5d4b1
HelloKitty <andrew.blakely@ymail.com>
2021-06-10 21:44:04 -0500

    Added EntityFrameworkGGDBFDataSource

---
+++ src/GGDBF.DataSource.EntityFramework/EntityFrameworkGGDBFDataSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GGDBF
+{
+	/// <summary>
+	/// EntityFramework <see cref="DbContext"/>-base implementation of <see cref="IGGDBFDataSource"/>.
+	/// </summary>
+	public sealed class EntityFrameworkGGDBFDataSource : IGGDBFDataSource
+	{
+		/// <summary>
+		/// Internal data source.
+		/// </summary>
+		private DbContext Context { get; }
+
+		public EntityFrameworkGGDBFDataSource(DbContext context)
+		{
+			Context = context ?? throw new ArgumentNullException(nameof(context));
+		}
+
+		/// <inheritdoc />
+		public async Task<IEnumerable<TModelType>> RetrieveAllAsync<TModelType>(CancellationToken token = default) 
+			where TModelType : class
+		{
+			return await Context
+				.Set<TModelType>()
+				.ToArrayAsync(token);
+		}
+	}
+}
