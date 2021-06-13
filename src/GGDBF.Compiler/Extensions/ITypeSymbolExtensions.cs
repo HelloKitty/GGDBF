15a6245ed79c81a47cf482c99d8f66b46ea6a8c4
HelloKitty <andrew.blakely@ymail.com>
2021-06-12 22:18:17 -0500

    Added ITypeSymbolExtensions and HasForeignKeyDefined

---
+++ src/GGDBF.Compiler/Extensions/ITypeSymbolExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using Glader.Essentials;
+using Microsoft.CodeAnalysis;
+
+namespace GGDBF
+{
+	public static class ITypeSymbolExtensions
+	{
+		public static bool HasForeignKeyDefined(this ITypeSymbol type)
+		{
+			return type.GetMembers().Any(m => m.HasAttributeLike<ForeignKeyPropertyMissingHintAttribute>())
+			       || type.GetMembers().Any(m => m.HasAttributeLike<ForeignKeyAttribute>())
+			       || type.GetMembers().Any(m => m.HasAttributeLike<ForeignKeyHintAttribute>());
+		}
+	}
+}
