f214eb23936c1c4466bdbab6829172d6867e83a1
HelloKitty <andrew.blakely@ymail.com>
2021-06-12 22:16:03 -0500

    Added ForeignKeyHintAttribute

---
+++ src/GGDBF.Common/Attributes/ForeignKeyHintAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF
+{
+	/// <summary>
+	/// Attribute marked on a model type that indicates a foreign key relationship exists
+	/// between the two properties.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Property)]
+	public sealed class ForeignKeyHintAttribute : Attribute
+	{
+		public string KeyProperty { get; }
+
+		public string NavigationProperty { get; }
+
+		public ForeignKeyHintAttribute(string keyProperty, string navigationProperty)
+		{
+			if (string.IsNullOrWhiteSpace(keyProperty)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(keyProperty));
+			if (string.IsNullOrWhiteSpace(navigationProperty)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(navigationProperty));
+
+			KeyProperty = keyProperty;
+			NavigationProperty = navigationProperty;
+		}
+	}
+}
