454d16b9572475437de11a64aba97f40d1142a26
HelloKitty <andrew.blakely@ymail.com>
2021-06-14 02:02:14 -0500

    Added Protobuf serializer

---
+++ src/GGDBF.Serializer.ProtobufNet/Serializers/DefaultGGDBFProtobufNetSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ProtoBuf;
+
+namespace GGDBF
+{
+	public sealed class DefaultGGDBFProtobufNetSerializer : IGGDBFSerializer
+	{
+		/// <inheritdoc />
+		public byte[] Serialize<TPrimaryKeyType, TModelType>(GGDBFTable<TPrimaryKeyType, TModelType> table)
+		{
+			//This allocates like HELL and is not efficient but this process is
+			//offline for running time of the applications (only generation)
+			//so ignore perf.
+			using (MemoryStream ms = new MemoryStream())
+			{
+				Serializer.Serialize(ms, table);
+				ms.Position = 0;
+				return ms.ToArray();
+			}
+		}
+	}
+}
