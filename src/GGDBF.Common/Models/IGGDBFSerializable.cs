e4c772eee5a68a5dfe7369fbd03555a156df91a9
HelloKitty <andrew.blakely@ymail.com>
2021-06-13 20:49:37 -0500

    Added IGGDBFSerializable

---
+++ src/GGDBF.Common/Models/IGGDBFSerializable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF
+{
+	/// <summary>
+	/// Contract that can be implemented on types that required post-initialization for
+	/// conversion/serialization.
+	/// </summary>
+	public interface IGGDBFSerializable
+	{
+		/// <summary>
+		/// Initializes a model into a state where it can be serialized by GGDBF.
+		/// </summary>
+		void Initialize();
+	}
+}
