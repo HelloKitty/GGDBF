af7ecd34654a5072698edab04904f1586dfe6183
HelloKitty <andrew.blakely@ymail.com>
2021-06-14 00:11:53 -0500

    Added GGDBFConstants

---
+++ src/GGDBF.Common/GGDBFConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF
+{
+	public static class GGDBFConstants
+	{
+		public const string FILE_EXTENSION_SUFFIX = "ggdbf";
+	}
+}
