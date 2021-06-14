672553942695b49a34d054288440b016aed43bff
HelloKitty <andrew.blakely@ymail.com>
2021-06-13 22:51:02 -0500

    Added IGGDBFWriteable

---
+++ src/GGDBF.Common/Models/IGGDBFWriteable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GGDBF
+{
+	public interface IGGDBFWriteable
+	{
+		Task WriteAsync(IGGDBFDataWriter writer, CancellationToken token = default);
+	}
+}
