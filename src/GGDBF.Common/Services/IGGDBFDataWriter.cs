8a02e55d1f7ee3e11ee5c70171ef0128982a1cb0
HelloKitty <andrew.blakely@ymail.com>
2021-06-13 22:51:07 -0500

    Added IGGDBFDataWriter

---
+++ src/GGDBF.Common/Services/IGGDBFDataWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GGDBF
+{
+	public interface IGGDBFDataWriter
+	{
+		Task WriteAsync<TPrimaryKeyType, TModelType>(GGDBFTable<TPrimaryKeyType, TModelType> table, CancellationToken token = default);
+	}
+}
