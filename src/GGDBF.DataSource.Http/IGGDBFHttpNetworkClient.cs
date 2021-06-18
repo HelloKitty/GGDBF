438d9b7bfed1730fb458f6ddf71135305b5472a4
HelloKitty <andrew.blakely@ymail.com>
2021-06-18 03:44:17 -0500

    Added IGGDBFHttpNetworkClient

---
+++ src/GGDBF.DataSource.Http/IGGDBFHttpNetworkClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GGDBF
+{
+	public interface IGGDBFHttpNetworkClient<TPrimaryKeyType, TModelType, TSerializableModelType>
+		where TSerializableModelType : class, TModelType
+	{
+		[Get("/api/GGDBF/{name}/{derivedType}")]
+		public Task<GGDBFTable<TPrimaryKeyType, TModelType>> RetrieveTableAsync([AliasAs("name")] string tableName, [AliasAs("derivedType")] string modelType, CancellationToken token = default);
+	}
+
+	public interface IGGDBFHttpNetworkClient<TPrimaryKeyType, TModelType>
+	{
+		[Get("/api/GGDBF/{name}")]
+		public Task<GGDBFTable<TPrimaryKeyType, TModelType>> RetrieveTableAsync([AliasAs("name")] string tableName, CancellationToken token = default);
+	}
+}
