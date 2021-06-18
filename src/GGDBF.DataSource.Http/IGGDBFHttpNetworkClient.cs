68e0a6783dc2dcacbb2f7a5d40d4981151a5aca0
HelloKitty <andrew.blakely@ymail.com>
2021-06-18 04:16:05 -0500

    Changed GGDBF Http paths parameter names

--- src/GGDBF.DataSource.Http/IGGDBFHttpNetworkClient.cs
+++ src/GGDBF.DataSource.Http/IGGDBFHttpNetworkClient.cs
@@ -9,11 +9,11 @@ namespace GGDBF
 {
 	public interface IGGDBFHttpNetworkClient
 	{
-		[Get("/api/GGDBF/{key}_{name}")]
-		public Task<GGDBFTable<TPrimaryKeyType, TModelType>> RetrieveTableAsync<TPrimaryKeyType, TModelType>([AliasAs("key")] string keyType, [AliasAs("name")] string tableName, CancellationToken token = default);
+		[Get("/api/GGDBF/{key}_{type}")]
+		public Task<GGDBFTable<TPrimaryKeyType, TModelType>> RetrieveTableAsync<TPrimaryKeyType, TModelType>([AliasAs("key")] string keyType, [AliasAs("type")] string modelType, CancellationToken token = default);
 
-		[Get("/api/GGDBF/{key}_{name}/{derivedType}")]
-		public Task<GGDBFTable<TPrimaryKeyType, TModelType>> RetrieveTableAsync<TPrimaryKeyType, TModelType, TSerializableModelType>([AliasAs("key")] string keyType, [AliasAs("name")] string tableName, [AliasAs("derivedType")] string modelType, CancellationToken token = default)
+		[Get("/api/GGDBF/{key}_{type}/{derivedType}")]
+		public Task<GGDBFTable<TPrimaryKeyType, TModelType>> RetrieveTableAsync<TPrimaryKeyType, TModelType, TSerializableModelType>([AliasAs("key")] string keyType, [AliasAs("type")] string modelType, [AliasAs("derivedType")] string modelType, CancellationToken token = default)
 			where TSerializableModelType : class, TModelType;
 	}
 }
--- src/GGDBF.DataSource.Http/RefitHttpGGDBFDataSource.cs
+++ src/GGDBF.DataSource.Http/RefitHttpGGDBFDataSource.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection;
 using System.Text;
 using System.Threading;
