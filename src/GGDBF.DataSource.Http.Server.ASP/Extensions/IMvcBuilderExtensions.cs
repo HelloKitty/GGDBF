dec9d154fe81924c85fc9f7b2112fb022109ae25
HelloKitty <andrew.blakely@ymail.com>
2021-06-18 05:01:41 -0500

    Added controller registeration extension with RegisterGGDBFController

---
+++ src/GGDBF.DataSource.Http.Server.ASP/Extensions/IMvcBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Glader.Essentials;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace GGDBF
+{
+	public static class IMvcBuilderExtensions
+	{
+		/// <summary>
+		/// Registers the general <see cref="GGDBFContentController"/> with the MVC
+		/// controllers. See controller documentation for what it does and how it works.
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <returns></returns>
+		public static IMvcBuilder RegisterGGDBFController(this IMvcBuilder builder)
+		{
+			if(builder == null) throw new ArgumentNullException(nameof(builder));
+
+			return builder
+				.RegisterController<GGDBFContentController>();
+		}
+	}
+}
