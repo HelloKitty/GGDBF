a9eb3f464dfa46dc8529fcce9173e29b183013ab
HelloKitty <andrew.blakely@ymail.com>
2021-06-18 23:10:42 -0500

    Use custom assembly resolver for GGDBF type loading in HTTP server

--- src/GGDBF.DataSource.Http.Server.ASP/Controllers/GGDBFContentController.cs
+++ src/GGDBF.DataSource.Http.Server.ASP/Controllers/GGDBFContentController.cs
@@ -27,7 +27,7 @@ namespace GGDBF
 
 			//WARNING: Not safe to expose in production https://stackoverflow.com/questions/23895563/is-it-safe-to-call-type-gettype-with-an-untrusted-type-name
 			var resultAwaitable = (Task)GetType().GetMethod(nameof(RetrieveTableAsync), new Type[]{ typeof(string), typeof(string), typeof(CancellationToken)})
-				.MakeGenericMethod(Type.GetType(keyType), Type.GetType(modelType))
+				.MakeGenericMethod(ResolveTypeForGGDBF(keyType), ResolveTypeForGGDBF(modelType))
 				.Invoke(this, new object[] { keyType, modelType, token });
 
 			await resultAwaitable;
@@ -48,7 +48,7 @@ namespace GGDBF
 
 			//WARNING: Not safe to expose in production https://stackoverflow.com/questions/23895563/is-it-safe-to-call-type-gettype-with-an-untrusted-type-name
 			var resultAwaitable = (Task)GetType().GetMethod(nameof(RetrieveTableAsync), new Type[] { typeof(string), typeof(string), typeof(string), typeof(CancellationToken) })
-				.MakeGenericMethod(Type.GetType(keyType), Type.GetType(modelType), Type.GetType(derivedType))
+				.MakeGenericMethod(ResolveTypeForGGDBF(keyType), ResolveTypeForGGDBF(modelType), ResolveTypeForGGDBF(derivedType))
 				.Invoke(this, new object[] { keyType, modelType, derivedType, token });
 
 			await resultAwaitable;
@@ -58,6 +58,11 @@ namespace GGDBF
 				.GetValue(resultAwaitable);
 
 			return Json(result);
+		}
+
+		private static Type ResolveTypeForGGDBF(string modelType)
+		{
+			return Type.GetType(modelType, GGDBFAssemblyResolver, null);
 		}
 
 		public async Task<GGDBFTable<TPrimaryKeyType, TModelType>> RetrieveTableAsync<TPrimaryKeyType, TModelType>(string keyType, string tableName, CancellationToken token = default) 
@@ -76,6 +81,18 @@ namespace GGDBF
 			//We can ignore parameters since this is generic and we're implementing using the generics and not the text types
 			return await DataSource
 				.RetrieveFullTableAsync<TPrimaryKeyType, TModelType, TSerializableModelType>(new TableRetrievalConfig<TPrimaryKeyType, TModelType>(), token);
+		}
+
+		//See: https://stackoverflow.com/a/33277079
+		//This resolver method will ignore version, cultures and keys to make loading more reasonable.
+		private static System.Reflection.Assembly GGDBFAssemblyResolver(System.Reflection.AssemblyName assemblyName)
+		{
+			assemblyName.Version = null;
+			assemblyName.CultureInfo = null;
+			assemblyName.CultureName = null;
+			assemblyName.SetPublicKey(null);
+
+			return System.Reflection.Assembly.Load(assemblyName);
 		}
 	}
 }
