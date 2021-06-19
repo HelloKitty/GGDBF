5370f03eb20858b1e58b80d684e395f1932979dd
HelloKitty <andrew.blakely@ymail.com>
2021-06-19 10:51:18 -0500

    Added Reload request to GGDBFHttpNetworkClient

--- src/GGDBF.DataSource.Http/IGGDBFHttpNetworkClient.cs
+++ src/GGDBF.DataSource.Http/IGGDBFHttpNetworkClient.cs
@@ -18,5 +18,16 @@ namespace GGDBF
 		public Task<GGDBFTable<TPrimaryKeyType, TModelType>> RetrieveTableAsync<TPrimaryKeyType, TModelType, TSerializableModelType>([AliasAs("key")] string keyType, [AliasAs("type")] string modelType, [AliasAs("derivedType")] string derivedModelType, CancellationToken token = default)
 			where TSerializableModelType : class, TModelType, IGGDBFSerializable
 			where TModelType : class;
+
+		/// <summary>
+		/// Reloads the remote data source forces the data to be up-to-date.
+		/// </summary>
+		/// <typeparam name="TGGDBFContextType">The context to refresh.</typeparam>
+		/// <param name="contextType">The context type name to reload.</param>
+		/// <param name="token">Cancel token.</param>
+		/// <returns></returns>
+		[Post("/api/GGDBF/{context}/reload")]
+		public Task ReloadContextAsync<TGGDBFContextType>([AliasAs("context")] string contextType, CancellationToken token = default)
+			where TGGDBFContextType : class, IGGDBFContext;
 	}
 }
--- src/GGDBF.DataSource.Http/RefitHttpGGDBFDataSource.cs
+++ src/GGDBF.DataSource.Http/RefitHttpGGDBFDataSource.cs
@@ -8,21 +8,29 @@ using Refit;
 
 namespace GGDBF
 {
+	public record RefitHttpGGDBFDataSourceOptions(bool RefreshOnFirstQuery = false);
+
 	/// <summary>
-	/// An HTTP <see cref="IGGDBFDataSource"/> implementation that uses the Refit <see cref="RefitHttpGGDBFDataSource"/> interface
+	/// An HTTP <see cref="IGGDBFDataSource"/> implementation that uses the Refit <see cref="IGGDBFHttpNetworkClient"/> interface
 	/// to source the data.
 	/// </summary>
-	public sealed class RefitHttpGGDBFDataSource : IGGDBFDataSource
+	public sealed class RefitHttpGGDBFDataSource<TGGDBFContextType> : IGGDBFDataSource
+		where TGGDBFContextType : class, IGGDBFContext
 	{
 		/// <summary>
 		/// The base URL for the requests.
 		/// </summary>
 		private string BaseUrl { get; }
 
-		public RefitHttpGGDBFDataSource(string baseUrl)
+		public RefitHttpGGDBFDataSourceOptions Options { get; }
+
+		private bool FirstQuery { get; set; } = true;
+
+		public RefitHttpGGDBFDataSource(string baseUrl, RefitHttpGGDBFDataSourceOptions options = null)
 		{
 			if (string.IsNullOrWhiteSpace(baseUrl)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(baseUrl));
 			BaseUrl = baseUrl;
+			Options = options ?? new RefitHttpGGDBFDataSourceOptions();
 		}
 
 		/// <inheritdoc />
@@ -36,6 +44,8 @@ namespace GGDBF
 		public async Task<GGDBFTable<TPrimaryKeyType, TModelType>> RetrieveFullTableAsync<TPrimaryKeyType, TModelType>(TableRetrievalConfig<TPrimaryKeyType, TModelType> config = null, CancellationToken token = default) 
 			where TModelType : class
 		{
+			await ReloadIfRequiredAsync<TPrimaryKeyType, TModelType>(token);
+
 			return await RestService
 				.For<IGGDBFHttpNetworkClient>(BaseUrl)
 				.RetrieveTableAsync<TPrimaryKeyType, TModelType>(typeof(TPrimaryKeyType).AssemblyQualifiedName, typeof(TModelType).AssemblyQualifiedName, token);
@@ -46,9 +56,24 @@ namespace GGDBF
 			where TModelType : class 
 			where TSerializableModelType : class, TModelType, IGGDBFSerializable
 		{
+			await ReloadIfRequiredAsync<TPrimaryKeyType, TModelType>(token);
+
 			return await RestService
 				.For<IGGDBFHttpNetworkClient>(BaseUrl)
 				.RetrieveTableAsync<TPrimaryKeyType, TModelType, TSerializableModelType>(typeof(TPrimaryKeyType).AssemblyQualifiedName, typeof(TModelType).AssemblyQualifiedName, typeof(TSerializableModelType).AssemblyQualifiedName, token);
+		}
+
+		private async Task ReloadIfRequiredAsync<TPrimaryKeyType, TModelType>(CancellationToken token = default) 
+			where TModelType : class
+		{
+			if (Options.RefreshOnFirstQuery && FirstQuery)
+			{
+				await RestService
+					.For<IGGDBFHttpNetworkClient>(BaseUrl)
+					.ReloadContextAsync<TGGDBFContextType>(typeof(TGGDBFContextType).AssemblyQualifiedName, token);
+
+				FirstQuery = true;
+			}
 		}
 	}
 }
