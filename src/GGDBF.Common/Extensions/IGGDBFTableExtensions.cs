0fde2d1c2b5802c4845a1226c102064f9d5570f9
HelloKitty <andrew.blakely@ymail.com>
2021-06-19 23:11:57 -0500

    Added GGDBFTableExtensions and ConvertTo

---
+++ src/GGDBF.Common/Extensions/IGGDBFTableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GGDBF
+{
+	public static class GGDBFTableExtensions
+	{
+		/// <summary>
+		/// Converts a <see cref="GGDBFTable{TPrimaryKeyType,TModelType}"/> to another <see cref="GGDBFTable{TPrimaryKeyType,TModelType}"/>
+		/// with a more derived typed value type.
+		/// </summary>
+		/// <typeparam name="TPrimaryKeyType">Primary table key.</typeparam>
+		/// <typeparam name="TModelType">Model type.</typeparam>
+		/// <typeparam name="TSerializableModelType">The derived model type.</typeparam>
+		/// <param name="table">The table to convert.</param>
+		/// <returns></returns>
+		public static GGDBFTable<TPrimaryKeyType, TSerializableModelType> ConvertTo<TPrimaryKeyType, TModelType, TSerializableModelType>(this GGDBFTable<TPrimaryKeyType, TModelType> table)
+			where TSerializableModelType : TModelType
+		{
+			if (table == null) throw new ArgumentNullException(nameof(table));
+
+			return new GGDBFTable<TPrimaryKeyType, TSerializableModelType>()
+			{
+				Version = table.Version,
+				TableName = table.TableName,
+				TableData = table.TableData.ToDictionary(kvp => kvp.Key, kvp => (TSerializableModelType) kvp.Value)
+			};
+		}
+	}
+}
