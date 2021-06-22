348e521e111b0d987b4000732e2ad3080f8f66f3
HelloKitty <andrew.blakely@ymail.com>
2021-06-22 10:01:38 -0500

    Added some test cases for ownedtype generic parameter matching

--- tests/GGDBF.Compiler.ManualTest/GeneratorOutput/GGDBF.Compiler/GGDBF.ContextGenerator/TestContextGenericConstraints.cs
+++ tests/GGDBF.Compiler.ManualTest/GeneratorOutput/GGDBF.Compiler/GGDBF.ContextGenerator/TestContextGenericConstraints.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.CodeDom.Compiler;
 using System.Runtime.Serialization;
 using GGDBF;
+using TestNamespace;
 using TestNamespace2;
 
 namespace GGDBF
@@ -13,6 +14,8 @@ namespace GGDBF
        where TAnotherType : class, System.Enum
        where TAnotherType2 : unmanaged
     {
+        public IReadOnlyDictionary<Int16, TestModelType> TestDatas { get; }
+
         public IReadOnlyDictionary<String, TestModelType2> Test2Datas { get; }
 
         public IReadOnlyDictionary<String, TestModelType4> Test4Datas { get; }
@@ -20,6 +23,8 @@ namespace GGDBF
         public IReadOnlyDictionary<TKey, TestModelType6<TKey>> Test6Datas { get; }
 
         public IReadOnlyDictionary<TestModelType9Key<TKey, TAnotherType>, TestModelType9<TKey, TAnotherType>> Test9Datas { get; }
+
+        public IReadOnlyDictionary<TKey, TestModelType15<TKey>> Test15Datas { get; }
 
     }
 
@@ -30,6 +35,8 @@ namespace GGDBF
     {
         public static TestContextGenericConstraints<TKey, TAnotherType, TAnotherType2> Instance { get; private set; }
 
+        public IReadOnlyDictionary<Int16, TestModelType> TestDatas { get; init; }
+
         public IReadOnlyDictionary<String, TestModelType2> Test2Datas { get; init; }
 
         public IReadOnlyDictionary<String, TestModelType4> Test4Datas { get; init; }
@@ -38,10 +45,12 @@ namespace GGDBF
         {
             Instance = new()
             {
+                TestDatas = await source.RetrieveTableAsync<Int16, TestModelType>(new NameOverrideTableRetrievalConfig<Int16, TestModelType>("TestDatas")),
                 Test2Datas = await source.RetrieveTableAsync<String, TestModelType2>(new NameOverrideTableRetrievalConfig<String, TestModelType2>("Test2Datas")),
                 Test4Datas = await source.RetrieveTableAsync<String, TestModelType4, TestContextGenericConstraints_TestModelType4<TKey, TAnotherType, TAnotherType2>>(new NameOverrideTableRetrievalConfig<String, TestModelType4>("Test4Datas")),
                 Test6Datas = await source.RetrieveTableAsync<TKey, TestModelType6<TKey>, TestContextGenericConstraints_TestModelType6<TKey, TAnotherType, TAnotherType2>>(new NameOverrideTableRetrievalConfig<TKey, TestModelType6<TKey>>("Test6Datas")),
                 Test9Datas = await source.RetrieveTableAsync<TestModelType9Key<TKey, TAnotherType>, TestModelType9<TKey, TAnotherType>, TestContextGenericConstraints_TestModelType9<TKey, TAnotherType, TAnotherType2>>(new NameOverrideTableRetrievalConfig<TestModelType9Key<TKey, TAnotherType>, TestModelType9<TKey, TAnotherType>>("Test9Datas") { KeyResolutionFunction = m => new TestModelType9Key<TKey, TAnotherType>(m.Id1, m.Id2) }),
+                Test15Datas = await source.RetrieveTableAsync<TKey, TestModelType15<TKey>, TestContextGenericConstraints_TestModelType15<TKey, TAnotherType, TAnotherType2>>(new NameOverrideTableRetrievalConfig<TKey, TestModelType15<TKey>>("Test15Datas")),
             };
         }
     }
