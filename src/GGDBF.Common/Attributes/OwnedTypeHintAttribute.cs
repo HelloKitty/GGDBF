c61f77b9032523591be27a265894f8acba76eba8
HelloKitty <andrew.blakely@ymail.com>
2021-06-21 20:54:51 -0500

    Added new unused OwnedTypeHintAttribute

---
+++ src/GGDBF.Common/Attributes/OwnedTypeHintAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF
+{
+	/// <summary>
+	/// Hints to GGDBF that this is an OwnedType but not a Table type
+	/// and should generate foreign key properties if required.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public sealed class OwnedTypeHintAttribute : Attribute
+	{
+
+	}
+}
