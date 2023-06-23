029277ea46574ea6d5e1df16d1c55d582c3211af
HelloKitty <andrew.blakely@ymail.com>
2023-06-23 10:26:08 -0500

    Added MutableModelTableAttribute

---
+++ src/GGDBF.Common/Attributes/MutableModelTableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF
+{
+	/// <summary>
+	/// Attribute that marks a model/table type as a mutable model.
+	/// This means that uses of GGDBF can add or remove entries from the table.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public sealed class MutableModelTableAttribute : Attribute
+	{
+
+	}
+}
