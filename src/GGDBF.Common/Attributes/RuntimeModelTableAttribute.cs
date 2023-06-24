b7d5e15dab92e1fb4ffb71cc9ab155597e8f4ed0
HelloKitty <andrew.blakely@ymail.com>
2023-06-24 01:37:28 -0500

    Added RuntimeModelTableAttribute

---
+++ src/GGDBF.Common/Attributes/RuntimeModelTableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF
+{
+	/// <summary>
+	/// Attribute that marks a model/table type as being runtime only.
+	/// This means that no table loading code will be generated for the table.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public sealed class RuntimeModelTableAttribute : Attribute
+	{
+
+	}
+}
