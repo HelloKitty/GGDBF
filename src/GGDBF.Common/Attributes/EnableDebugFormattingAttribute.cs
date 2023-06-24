e50b6ad07938d372013fab16608e0f474f5befc3
HelloKitty <andrew.blakely@ymail.com>
2023-06-24 02:16:53 -0500

    Added EnableDebugFormattingAttribute

---
+++ src/GGDBF.Common/Attributes/EnableDebugFormattingAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGDBF
+{
+	/// <summary>
+	/// Attribute that enables pretty C#-IDE like auto-formatting for generated source.
+	/// This is useful for debugging and inspecting the generated source code.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public sealed class EnableDebugFormattingAttribute : Attribute
+	{
+
+	}
+}
